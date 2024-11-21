6503a4cf31ba7d92834fdf1b3a166b10b2af5c22
Lucas Bertuleza <lucascbertuleza@gmail.com>
2024-11-21 16:05:21 +0000

    chore: setup TypeOrm with Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const PostgresConnection = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'database',
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: process.env.NODE_ENV === 'development',
+  logging: true,
+  entities: [],
+  migrations: [],
+});
+
 @Module({
-  imports: [],
+  imports: [PostgresConnection],
   controllers: [AppController],
   providers: [AppService],
 })
