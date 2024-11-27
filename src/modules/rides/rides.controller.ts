900fd16eac1ed673a0048e478bbcd9f5778fdd78
Lucas Bertuleza <lucascbertuleza@gmail.com>
2024-11-27 23:50:31 +0000

    feat: enable `estimate` action

--- src/modules/rides/rides.controller.ts
+++ src/modules/rides/rides.controller.ts
@@ -1,9 +1,20 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { RidesService } from './rides.service';
+import { PartnerDriversService } from '@modules/partner-drivers/partner-drivers.service';
 import { RideEstimateDto } from './ride-estimate.dto';
 
 @Controller('ride')
 export class RidesController {
+  constructor(
+    private readonly ridesService: RidesService,
+    private readonly partnerDriversService: PartnerDriversService,
+  ) {}
+
   @Post('estimate')
   @HttpCode(200)
-  estimate(@Body() estimateDto: RideEstimateDto) {}
+  async estimate(@Body() { origin, destination }: RideEstimateDto) {
+    const distance = await this.ridesService.computeRoute(origin, destination);
+    const availableDrivers = await this.partnerDriversService.findAvailablesFromDistance(distance);
+    return this.ridesService.getRideWith(availableDrivers);
+  }
 }
--- src/modules/rides/rides.module.ts
+++ src/modules/rides/rides.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RidesController } from './rides.controller';
+import { HttpModule } from '@nestjs/axios';
+import { RidesService } from './rides.service';
+import { GoogleMapsRouting } from 'src/clients/google/routes-api';
+import { PartnerDriversModule } from '@modules/partner-drivers/partner-drivers.module';
 
 @Module({
+  imports: [HttpModule, PartnerDriversModule],
   controllers: [RidesController],
+  providers: [RidesService, GoogleMapsRouting],
 })
 export class RidesModule {}
