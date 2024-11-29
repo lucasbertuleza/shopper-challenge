a17e67aae3c196af0ecde9000601f1fce7c28e87
Lucas Bertuleza <lucascbertuleza@gmail.com>
2024-11-29 04:51:11 +0000

    feat: enable dependency injection in validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ValidationFailed } from './modules/app/validation-failed';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({ transform: true, whitelist: true, exceptionFactory: ValidationFailed }),
   );
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
